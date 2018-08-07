54a143c286287d2d948f48f55eda7a6767824f2b
Luan Tran <Luan@moon-knight.attlocal.net>
2018-08-06 19:19:12 -0700

    Created P/Q and M

--- BBS.c
+++ BBS.c
@@ -11,7 +11,8 @@ int main( int argc,  char * argv[]){
    
    //Check for arguments
    if( argc != NUMARGS){
-      printf("Incorrect number of arguments\n",stderr);
+      printf("Incorrect number of arguments, expected two arguments.\n",stderr);
+      exit(0);
    }
    
    char * randSeed;
@@ -26,14 +27,37 @@ int main( int argc,  char * argv[]){
    //Generate two prime numbers
    BIGNUM *primeP = BN_new();
    BIGNUM *primeQ = BN_new();
+   BN_CTX *ctx = BN_CTX_new();
    BN_ULONG four = 4;
    RAND_seed(randSeed, sizeof(randSeed));
    
    //Check primeP is congruent 3 mod 4
-   BN_generate_prime_ex(primeP,512,0, NULL,NULL,NULL);
+   BN_generate_prime_ex(primeP,LENGTH,0, NULL,NULL,NULL);
    while(  BN_mod_word(primeP,four) != 3){
-      BN_generate_prime_ex(primeP,512,0, NULL,NULL,NULL);
+      BN_generate_prime_ex(primeP,LENGTH,0, NULL,NULL,NULL);
    }
+
+   //Check primeQ is congruent 3 mod 4 and not equal to PrimeP
+   BN_generate_prime_ex(primeQ,LENGTH,0,NULL,NULL,NULL);
+   while( BN_mod_word(primeQ,four) != 3 & BN_cmp(primeP,primeQ) != 0){
+      BN_generate_prime_ex(primeQ,LENGTH,0,NULL,NULL,NULL);
+   }
+   
+   //Create Blum integer
+   BIGNUM * blumInt = BN_new();
+   BN_mul(blumInt,primeP,primeQ,ctx);
+
+   
+   //Create x, where gcd(x,blumInt) == 1
+   BIGNUM * seed = BN_new();
+   BIGNUM * rem = BN_new();
+   BN_generate_prime_ex(seed,LENGTH,0,NULL,NULL,NULL);
+   BN_gcd(rem,seed,blumInt,ctx);
+   while( BN_is_one(rem) != 1){
+      BN_generate_prime_ex(seed,LENGTH,0,NULL,NULL,NULL);
+      BN_gcd(rem,seed,blumInt,ctx);
+   
+      
 
   
   /*
